5ffa6f7ed09eb6c40fb6ab9ef574761002ca4649
Willinas Costa <prwillians.costa@gmail.com>
2023-11-14 17:47:59 -0300

    desafio celular concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,28 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
+         public Smartphone(string numero, string modelo,string imei,int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        // TODO: Passar os parâmetros do construtor para as propriedades
         }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
+        }
+
+
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero : "1234565", modelo:"modelo1",imei:"1111111111", memoria: 65);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero : "1234565", modelo:"modelo1",imei:"1111111111", memoria: 65);
+nokia.Ligar();
+nokia.InstalarAplicativo("telegram");
+
+
+
+
+
+
+
+
+
+
+
